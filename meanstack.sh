021ec47e0680a27d4ca73fe58bfc829f3f2f6423
mansoorkodungallur <mansoorkodungallur@gmail.com>
2018-05-11 18:20:29 +0530

    Update meanstack.sh

--- meanstack.sh
+++ meanstack.sh
@@ -2,8 +2,8 @@
 #script to install meanstack on ubunut 16 lts
 
 #installing  vim terminator
-sudo apt-get update
-sudo apt-get install vim terminator
+sudo apt-get update -y
+sudo apt-get install vim terminator -y 
 
 
 #installing mongodb
@@ -14,27 +14,27 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt-get update -y
 sudo apt-get install -y mongodb-org
-service mongod status
+sudo service mongod status
 
 
 #to install nvm  and npm
-curl -o https://raw.githubusercontent.com/creationix/nvm/master/install.sh
-sudo bash install.sh
+curl -o https://raw.githubusercontent.com/creationix/nvm/master/install.sh install0.sh
+sudo bash install0.sh
 sudo apt-get install -y build-essential
-curl https://npmjs.org/install.sh 
-bash install.sh 
+curl -o  https://npmjs.org/install.sh install1.sh 
+sudo bash install1.sh 
 
 #installing nodejs
-nvm install 8.9.1
-nvm use 8.9.1
-npm init
-npm install -g npm-check
-npm install -g @angular/cli
-npm install -g @angular/cli@latest
-npm install -g typescript
-npm install angular --save
-npm install express --save
-npm link express 
+ nvm install 8.9.1
+ nvm use 8.9.1
+ npm init
+ npm install -g npm-check
+ npm install -g @angular/cli
+ npm install -g @angular/cli@latest
+ npm install -g typescript
+ npm install angular --save
+ npm install express --save
+ npm link express 
 #instaling node js
 #echo starting nodejs installation
 #curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
@@ -46,7 +46,7 @@ npm link express
 
 #installing npm boswer gulp
 #sudo npm install -g bower 
-sudo npm install -g gulp
+npm install -g gulp
 
  #ng new helloApp
  #ng serve --open
