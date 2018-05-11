6b6284b8ac298fb1b133359ecc2ca0e40fab2b84
mansoorkodungallur <mansoorkodungallur@gmail.com>
2018-05-11 18:11:56 +0530

    some changes

--- meanstack.sh
+++ meanstack.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-script to install meanstack on ubunut 16 lts
+#script to install meanstack on ubunut 16 lts
+
+#installing  vim terminator
+sudo apt-get update
+sudo apt-get install vim terminator
 
 
 #installing mongodb
-echo starting mongodb installation
+echo "--------------------------starting mongodb installation-------------------------------"
 sudo apt-get install git -y
 sudo apt-get install build-essential -y
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
@@ -13,22 +17,40 @@ sudo apt-get install -y mongodb-org
 service mongod status
 
 
+#to install nvm  and npm
+curl -o https://raw.githubusercontent.com/creationix/nvm/master/install.sh
+sudo bash install.sh
+sudo apt-get install -y build-essential
+curl https://npmjs.org/install.sh 
+bash install.sh 
 
+#installing nodejs
+nvm install 8.9.1
+nvm use 8.9.1
+npm init
+npm install -g npm-check
+npm install -g @angular/cli
+npm install -g @angular/cli@latest
+npm install -g typescript
+npm install angular --save
+npm install express --save
+npm link express 
 #instaling node js
-
-echo starting nodejs installation
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs npm
+#echo starting nodejs installation
+#curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+#sudo apt-get install -y nodejs npm
 #instaling the rest of depentancies
-
-git clone https://github.com/linnovate/mean.git
-cd mean
-npm install
+#git clone https://github.com/linnovate/mean.git
+#cd mean
+#npm install
 
 #installing npm boswer gulp
-
 #sudo npm install -g bower 
 sudo npm install -g gulp
+
+ #ng new helloApp
+ #ng serve --open
+
 
 
 ~                               
